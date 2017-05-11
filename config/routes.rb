c1868c35331cc2eac23ec927dd9707afa4afd323
lsm1993 <guan2359@gmail.com>
2017-05-11 09:42:35 +0000

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get 'static_pages/about'
 
   root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
