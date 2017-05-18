4b366da5c18613181c15a4d99413f5e2c75be20d
lsm1993 <guan2359@gmail.com>
2017-05-18 08:35:06 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 处理注册成功的情况
+      log_in @user
       flash[:success] = "注册成功！"
       redirect_to user_url(@user)
     else
